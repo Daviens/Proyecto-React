376dab677e07003a1f4dbfc37203e90fcc2f6e87
Fernando Flekenstein <fer.flekenstein96@gmail.com>
2023-05-26 03:17:49 -0300

    change credentials to include

--- src/services/userService.js
+++ src/services/userService.js
@@ -20,7 +20,7 @@ export default class UserService {
       method: 'post',
       body: data,
       config: {
-        'withCredentials': 'true',
+        'withCredentials': 'include',
         'Content-Type': 'application/json'
       }
     }
@@ -30,7 +30,7 @@ export default class UserService {
     const info = {
       url: `https://ecommapi-production-ce2e.up.railway.app/api/sessions/islogin`,
       config:{
-        'withCredentials': 'true',
+        'withCredentials': 'include',
         'Content-Type': 'application/json'
       }
     }
